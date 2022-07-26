4ac931911f13da0a14c1127a558be058b269743c
Tomas Pocius <pocius.tomastp@gmail.com>
2022-07-26 18:14:47 +0300

    added footer and navbar for mobile positioned absolute

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,14 @@
+//navbar for desktop and tablet
 const navBar = (() => {
   let nav = $(function(){
     $(".navbar").load("navbar.html");
+  });
+  return {nav};
+})();  
+//navbar for mobile
+const navBarMob = (() => {
+  let nav = $(function(){
+    $(".navbar-mobile").load("navbar-mobile.html");
   });
   return {nav};
 })();  
