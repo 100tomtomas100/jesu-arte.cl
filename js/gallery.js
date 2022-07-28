cd3578ca66a113f27e35423059298905130fb42a
Tomas Pocius <pocius.tomastp@gmail.com>
2022-07-28 19:20:56 +0300

    gallery mobile added

--- js/gallery.js
+++ js/gallery.js
@@ -30,59 +30,58 @@ const gallery = (() => {
             const forward = (() => { 
                 if (counter === 1) {
                     if (mainPainting - 1 === 0){
-                        delPainting = allPaintings.length - 1;
-                        prevPainting = mainPainting - 1;
                         nextPainting = mainPainting + 1;
+                        delPainting = allPaintings.length - 1;
+                        prevPainting = mainPainting - 1;                        
                     }
                     if (mainPainting === 0) {
-                        delPainting = allPaintings.length - 2;
-                        prevPainting = allPaintings.length - 1;
                         nextPainting = mainPainting + 1;
+                        delPainting = allPaintings.length - 2;
+                        prevPainting = allPaintings.length - 1;                        
                     }
                     if (mainPainting === allPaintings.length -1) {
-                        delPainting = mainPainting - 2;
-                        prevPainting = mainPainting - 1;
                         nextPainting = 0;
+                        delPainting = mainPainting - 2;
+                        prevPainting = mainPainting - 1;                        
                     }          
                     if (mainPainting -1 != 0 && mainPainting != allPaintings.length - 1
                         && mainPainting != 0) {
+                        nextPainting = mainPainting + 1;
                         delPainting = mainPainting -2;
-                        prevPainting = mainPainting -1;
-                        nextPainting = mainPainting + 1
+                        prevPainting = mainPainting -1;                        
                     }
                 }
             })();  
             const backwards = (() => {
                 if (counter === -1) {
                     if (mainPainting === 0) {
-                        delPainting = mainPainting + 2;
-                        prevPainting = allPaintings.length - 1;
                         nextPainting = mainPainting + 1;
+                        delPainting = mainPainting + 2;
+                        prevPainting = allPaintings.length - 1;                        
                     }
                     if (mainPainting === allPaintings.length - 1){
-                        delPainting = 1;
-                        prevPainting = mainPainting - 1; 
                         nextPainting = 0;
+                        delPainting = 1;
+                        prevPainting = mainPainting - 1;                         
                     }
                     if (mainPainting + 1 === allPaintings.length - 1) {
-                        delPainting = 0;
-                        prevPainting = mainPainting - 1;
                         nextPainting = mainPainting +1;
+                        delPainting = 0;
+                        prevPainting = mainPainting - 1;                        
                     }
                     if (mainPainting !=0 && mainPainting != allPaintings.length - 1
                         && mainPainting + 1 != allPaintings.length - 1) {
-                        delPainting = mainPainting + 2;
-                        prevPainting = mainPainting - 1;
                         nextPainting = mainPainting + 1;
+                        delPainting = mainPainting + 2;
+                        prevPainting = mainPainting - 1;                        
                     } 
                 }
-
             })();
-            const rotator = (() => {                     
-                [...allPaintings][mainPainting].dataset.active = "main";
-                [...allPaintings][nextPainting].dataset.active = "next";
-                [...allPaintings][prevPainting].dataset.active = "prev";
-                [...allPaintings][delPainting].dataset.active = ""; 
+            const rotator = (() => {
+                [...allPaintings][mainPainting].dataset.active = "main";  
+                [...allPaintings][nextPainting].dataset.active = "next";   
+                [...allPaintings][prevPainting].dataset.active = "prev";                                          
+                [...allPaintings][delPainting].dataset.active = "";                
             })();         
         })
     })
