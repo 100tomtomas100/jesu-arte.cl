a88fbb38ae6ea9400856d03bf999cbba087d6b27
Tomas Pocius <pocius.tomastp@gmail.com>
2022-08-15 15:42:39 +0300

    changed gallery background color

--- js/navbar.js
+++ js/navbar.js
@@ -11,4 +11,22 @@ const navBarMob = (() => {
     $(".navbar-mobile").load("navbar-mobile.html");
   });
   return {nav};
-})();  
+})();
+
+const navbarBackground = (() => {
+  const navbarMobil = document.querySelector('.navbar-mobile');
+  const navbar = document.querySelector('.navbar');
+window.onscroll = () => {
+  if (window.scrollY > 100) {
+      navbar.style.background = "rgb(234, 242, 248, 0.5)";
+      navbarMobil.style.background = "rgb(234, 242, 248, 0.5)";
+  } else {
+      navbar.style.backgroundColor = "transparent";
+      navbarMobil.style.backgroundColor = "transparent";
+  }
+  //navbar to close on scrolling
+  if (window.scrollY > 0 && document.getElementById("navbar-mobile-buttons").style.display === "") {
+      document.getElementById("navbar-mobile-buttons").style.display = "none";
+  }
+};
+})();
